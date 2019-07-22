a89f61aa65cbd4d05d2a1c554eb67fed36941ec8
Ted Cook <teodoro.cook@gmail.com>
2019-07-21 23:05:49 -0600

    Fix testinfra deprecation errors

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,10 +6,10 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_command(Command):
-    assert Command('git --version').rc == 0
-    assert Command('gcc --version').rc == 0
-    assert Command('g++ --version').rc == 0
-    assert Command('make --version').rc == 0
-    assert Command('m4 --version').rc == 0
-    assert Command('patch --version').rc == 0
+def test_command(host):
+    assert host.command('git --version').rc == 0
+    assert host.command('gcc --version').rc == 0
+    assert host.command('g++ --version').rc == 0
+    assert host.command('make --version').rc == 0
+    assert host.command('m4 --version').rc == 0
+    assert host.command('patch --version').rc == 0
